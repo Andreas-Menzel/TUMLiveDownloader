4b8371c5b53e9becf0ff51780906b03413d48d3e
Andreas Menzel <mail@andreas-menzel.com>
2022-06-13 20:52:50 +0200

    Added basic graphical user interface

--- code/TUMLiveDownloader.py
+++ code/TUMLiveDownloader.py
@@ -2,6 +2,9 @@ import subprocess
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
+import PySimpleGUI as sg
+
+sg.theme('DarkAmber')
 
 # {
 #     <url>: <filename>, ...
@@ -45,6 +48,71 @@ def terminal_ui():
     download_lectures()
 
 
+def graphical_ui():
+    driver = webdriver.Firefox()
+    driver.get('https://live.rbg.tum.de')
+
+    filenames_list = 'The selected lectures will be listed here...'
+
+    layout_main = [
+        [sg.Text(filenames_list, key='_LECTURES_LIST_')],
+        [sg.Button('Add lecture'), sg.Button('Download lectures')]
+    ]
+
+    window_main = sg.Window('TUMLiveDownloader', layout_main)
+
+    while True:
+        event_main, values_main = window_main.read()
+        if event_main == sg.WIN_CLOSED:
+            exit()
+        elif event_main == 'Add lecture':
+            try:
+                video = driver.find_element(By.CSS_SELECTOR, '#watchContent video source')
+
+                url = video.get_attribute("src")
+                print(f'URL: {url}')
+
+                filename = None
+                layout_filename = [
+                    [sg.Text(f'URL: {url}')],
+                    [sg.Text('filename (without extension)'), sg.Input(key='_FILENAME_')],
+                    [sg.Button('Add lecture'), sg.Button('Cancel')]
+                ]
+                window_filename = sg.Window('Specify filename', layout_filename)
+                while True:
+                    event_filename, values_filename = window_filename.read()
+                    if event_filename == sg.WIN_CLOSED or event_filename == 'Cancel':
+                        break
+                    elif event_filename == 'Add lecture':
+                        filename = values_filename['_FILENAME_']
+                        break
+                window_filename.close()
+
+                if filename is None:
+                    print('Canceled.')
+                    continue
+
+                filename = filename + '.mp4'
+                print(f'filename: {filename}')
+                lectures[url] = filename
+
+                if len(lectures) > 0:
+                    filenames_list += '\n' + filename
+                else:
+                    filenames_list = filename
+                window_main['_LECTURES_LIST_'].update(filenames_list)
+
+                print('Added lecture.')
+            except NoSuchElementException:
+                print('No video found! Open a lecture first (pretend you want to watch it now).')
+        elif event_main == "Download lectures":
+            break
+
+    driver.close()
+    window_main.close()
+    download_lectures()
+
+
 # download_lectures
 #
 # @param    dict    _lectures       Dictionary that maps lecture urls
@@ -59,6 +127,7 @@ def download_lectures():
 
 
 if __name__ == '__main__':
-    terminal_ui()
+    #terminal_ui()
+    graphical_ui()
     input()
     print('Goodbye!')
